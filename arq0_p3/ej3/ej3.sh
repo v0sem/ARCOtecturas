5ab0f4dc55b42ace7159ed41304847a42fa3f3ab
v0sem <progpabsanch@gmail.com>
2019-11-26 12:57:47 +0100

    ej3

--- arq0_p3/ej3/ej3.sh
+++ arq0_p3/ej3/ej3.sh
@@ -5,12 +5,11 @@
 P=$((3%7+4))
 
 #Variables ej 1
-N1=$((10000 + 1024 * P))
-N2=$((10000 + 1024 * (P+1)))
+N1=$((256 + 256 * P))
+N2=$((256 + 256 * (P+1)))
 N=0
-Nstep=64
-Ndata=time_slow_fast.dat
-Nplot=time_slow_fast.png
+Nstep=16
+Ndata=mult.dat
 
 rm -f $Ndata $Nplot
 touch $Ndata
@@ -23,8 +22,8 @@ for x in {0..11}; do
 			times_slow[$i]=0
 			times_fast[$i]=0
 		fi;
-		slowTime_t=$(./slow "$N" | grep 'time' | awk '{print $3}')
-		fastTime_t=$(./fast "$N" | grep 'time' | awk '{print $3}')
+		slowTime_t=$(./mult_matrix "$N" | grep 'time' | awk '{print $3}')
+		fastTime_t=$(./mult_trans "$N" | grep 'time' | awk '{print $3}')
 	
 		times_slow[$i]=$(echo "${times_slow[$i]}" "$slowTime_t" | awk '{print $1 + $2}')
 		times_fast[$i]=$(echo "${times_fast[$i]}" "$fastTime_t" | awk '{print $1 + $2}')
@@ -35,12 +34,12 @@ done
 length=${#times_slow[@]}
 
 for ((N = 0; N < length; N += 1 )); do
-
+    Nsize=$((N1 + Nstep * N))
 	echo "$N"
 
 	slowTime=$(echo "${times_slow[$N]}" | awk '{print $1 / 12}')
 	fastTime=$(echo "${times_fast[$N]}" | awk '{print $1 / 12}')
 
-	echo "$N $slowTime $fastTime" >> $Ndata
+	echo "$Nsize $slowTime $fastTime" >> $Ndata
 done
 	
